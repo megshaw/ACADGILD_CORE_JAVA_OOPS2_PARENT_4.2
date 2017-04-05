de78e83f1e3440e47c32b561ccf8323a6f66cc94
meghana <meghana@Namsystech-Dev-01>
2017-04-05 16:42:41 +0530

    Java Assignment 4.2

---
+++ JavaAssignment 4.2/src/javaassignment/pkg4/pkg2/Parent.java
@@ -0,0 +1,35 @@
+package javaassignment.pkg4.pkg2;
+
+class Parent {
+
+    private void method1() {
+        System.out.println("Parent's method1()");
+    }
+
+    public void method2() {
+        System.out.println("Parent's method2()");
+        method1();
+    }
+}
+
+class Child extends Parent {
+
+    public void method1() {
+        System.out.println("Child's method1()");
+    }
+
+    public static void main(String args[]) {
+        Parent p = new Child();
+        p.method2();
+    }
+}
+
+
+
+/*Explanations *:
+
+The programmes output here is 
+Parent's method2()
+Parent's method1()
+
+*/
